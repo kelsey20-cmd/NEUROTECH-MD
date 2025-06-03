98fc45db4884585f388ecb0c6216a13d5f879462
Neurotech-xmd <onyariallamano@gmail.com>
2025-06-03 21:26:01 +0300

    Update mesfonctions.js

--- keizzah/mesfonctions.js
+++ keizzah/mesfonctions.js
@@ -34,7 +34,7 @@ module.exports.genererNomFichier = async (extension) => {
 /** ************ */
 module.exports.stick = async (buffer, author) => {
     var sticker = new Sticker(buffer, {
-        pack: 'ALPHA-MD',
+        pack: 'NEUROTECH-MD',
         author: author,
         type: StickerTypes.FULL,
         categories: ['🤩', '🎉'],
